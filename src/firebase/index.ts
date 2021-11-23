863e4d6d5f7d4edcac29b3718c221637e826a725
Domino731 <d.orzechowski18@gmail.com>
2021-11-23 21:49:59 +0100

    setup - firebase backend

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRDCeZUaSCc0shSYK4rleIAwRjxROit9I",
+  authDomain: "infinite-stack-e14f5.firebaseapp.com",
+  projectId: "infinite-stack-e14f5",
+  storageBucket: "infinite-stack-e14f5.appspot.com",
+  messagingSenderId: "599396404798",
+  appId: "1:599396404798:web:c31fab580890ae80530b6f",
+  measurementId: "G-N6MX4DXDVJ",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export {};
